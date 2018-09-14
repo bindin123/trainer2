e87de5e321aecaf0cb29d114a70c020f66b85ca2
Wreat Reert <petroniusz2008@gmail.com>
2018-09-14 16:37:00 +0200

    Dodaj pliki projektów.

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+
+extern int UImain();
+
+
+DWORD WINAPI Main_Thread(LPVOID lpParam) {
+	UIMain();
+	return S_OK;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD _reason, LPVOID lpReserved) {
+	if (_reason == DLL_PROCESS_ATTACH) {
+		CreateThread(0, 0x1000, &Main_Thread, 0, 0, NULL);
+	}
+}
